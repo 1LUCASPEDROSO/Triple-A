bf4d5c3d31705f4fd9f70b087b3e96a8177d3d64
lucas pedroso <pedrosolucas1745@gmail.com>
2023-09-30 18:28:10 -0300

    subindo versão com variaveis de ambiente

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = require('./router')
 const app = express();
+require('dotenv').config()
+const port = process.env.PORT
  app.use(router)
-app.listen(3,()=>{console.log('server running at port 3')})
+app.listen(port,()=>{console.log(`server running at port ${port}`)})
--- backend/src/router.js
+++ backend/src/router.js
@@ -2,5 +2,5 @@ const express = require('express')
 
 const router = express.Router()
 router.get('/',(req,res)=>res.status(200).send("router funcionando"))
-router.get('/gamesSony')
+router.get('/games')
 module.exports = router
